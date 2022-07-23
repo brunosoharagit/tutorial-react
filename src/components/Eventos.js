c4992cb78afb6c8bd4390622f37cb223288bc5b6
b.sohara.git <b.sohara.git@gmail.com>
2022-07-23 13:46:56 -0300

    TUTORIAL 2022.07.23 >> metodos por props

--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -1,14 +1,20 @@
 import PropTypes from 'prop-types';
+import Button from './events/Button';
 
-function Eventos({ numero = 0 }) {
+function Eventos() {
     const meuEvento = () => {
-        console.log(`Fui ativado!! Número ${numero}`);
-        numero++;
+        console.log(`Fui ativado!`);
+    }
+
+    function segundoEvento() {
+        console.log("Ativando o segundo elemento");
     }
 
     return (
         <div>
             <p>Cluqie para disparar o evento:</p>
+            <Button event={meuEvento} text="Primeiro Evento"/>
+            <Button event={segundoEvento} text="Segundo Evento"/>
             <button onClick={meuEvento}>Ativar!</button>
         </div>
     );
